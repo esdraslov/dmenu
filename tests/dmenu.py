c6f1ee766c8b69c47682e140a22188d50fb181c5
esdraslov <edvan.marcelino56@gmail.com>
2023-08-08 17:33:33 -0300

    d

--- tests/dmenu.py
+++ tests/dmenu.py
@@ -6,3 +6,9 @@ def _open_gameAssets(directory: str):
 
 def open_custom_assets(full_path: str):
     try_att = _open_gameAssets(full_path)
+def _open_menu(Tk: win_form.Tk):
+	Tk.mainloop()
+def OMBK(key: str, Tk: win_form.Tk):
+	if keyboard.is_pressed(key):
+		_open_menu(Tk)
+        
