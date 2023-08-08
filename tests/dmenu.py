6ac90cee270ea37dbe197a426bc6061d333c9dae
esdraslov <edvan.marcelino56@gmail.com>
2023-08-08 17:35:45 -0300

    fix

--- tests/dmenu.py
+++ tests/dmenu.py
@@ -1,5 +1,5 @@
 import tkinter as win_form
-import os
+import os, keyboard
 
 def _open_gameAssets(directory: str):
     return os.system(f"explorer.exe {directory}")
@@ -11,4 +11,3 @@ def _open_menu(Tk: win_form.Tk):
 def OMBK(key: str, Tk: win_form.Tk):
 	if keyboard.is_pressed(key):
 		_open_menu(Tk)
-        
