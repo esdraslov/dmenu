dcd6dcb3e6b9ecea29c302ebe6515de9573d3bf8
esdraslov <edvan.marcelino56@gmail.com>
2023-08-08 17:30:36 -0300

    more tools

--- dmenu.py
+++ dmenu.py
@@ -1,8 +1,13 @@
 import tkinter as win_form
-import os
+import os, keyboard
 
 def _open_gameAssets(directory: str):
     return os.system(f"explorer.exe {directory}")
 
 def open_custom_assets(full_path: str):
     try_att = _open_gameAssets(full_path)
+def _open_menu(Tk: win_form.Tk):
+	Tk.mainloop()
+def OMBK(key: str, Tk: win_form.Tk):
+	if keyboard.is_pressed(key):
+		_open_menu(Tk)
--- tests/test.py
+++ tests/test.py
@@ -2,8 +2,13 @@ import dmenu
 import tkinter
 import pytest
 import os
+import keyboard
+
+tk = dmenu.win_form.Tk()
 
 def test1():
     assert dmenu.win_form == tkinter
 def test2():
     dmenu.open_custom_assets(f"C:\\Users\\HP\\Documents\\dmenu\\tests\\assets_fake")
+def test3():
+    dmenu.OMBK("F1", tk)
